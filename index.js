e3c1816f3ed174afca850a04f4639d9496e7eb8f
Jonathan-Asher <joni062114@gmail.com>
2022-05-13 16:39:31 +0300

    Init

---
+++ index.js
@@ -0,0 +1,71 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+const url = process.argv[2];
+
+if (!url) {
+    throw "Please provide a URL as the first argument";
+}
+
+async function run () {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  await uploadFileToPage(page);
+  await savePageToFile(page);
+  await page.screenshot({path: 'screenshot.png'});
+  browser.close();
+}
+
+async function uploadFileToPage(page, filename = 'data') {
+  var data = await fileToObject(filename);
+  if(data.cookies)
+    await page.setCookie(...data.cookies);
+  await page.evaluate((data) => {
+    if(data.sessionStorage)
+      Object.entries(data.sessionStorage).forEach(([key,val]) => window.sessionStorage.setItem(key, val))
+  }, data)
+  await page.evaluate((data) => {
+    if(data.localStorage)
+      Object.entries(data.localStorage).forEach(([key,val]) => window.localStorage.setItem(key, val))
+  }, data)
+}
+
+async function fileToObject(filename) {
+  let obj;
+  if(filename.indexOf('.') != -1)
+    obj = JSON.parse(fs.readFileSync(filename));
+  else
+    obj = JSON.parse(fs.readFileSync(filename+'.json'));
+  return obj;
+}
+
+async function savePageToFile(page, filename = 'data') {
+  const sessionStorage = await page.evaluate(() => {
+    var vals = {}
+    Object.keys(window.sessionStorage).forEach(key => vals[key] = window.sessionStorage.getItem(key))
+    return vals;
+  })
+  const localStorage = await page.evaluate(() => {
+    var vals = {}
+    Object.keys(window.localStorage).forEach(key => vals[key] = window.localStorage.getItem(key))
+    return vals;
+  })
+  const result = {
+    url: page.url(),
+    cookies: await page.cookies(),
+    sessionStorage,
+    localStorage
+  }
+  objectToFile(result, filename);
+}
+
+async function objectToFile(obj, filename) {
+  let data = JSON.stringify(obj);
+  if(filename.indexOf('.') != -1)
+    fs.writeFileSync(filename, data);
+  else
+    fs.writeFileSync(filename+'.json', data);
+}
+
+run();
